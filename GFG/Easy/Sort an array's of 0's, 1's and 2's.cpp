fa5e02337930e2473b0849e90d9169b2bf6b7521
sadhvibs <76248658+sadhvibs@users.noreply.github.com>
2022-10-09 22:01:17 +0530

    .init

---
+++ GFG/Easy/Sort an array's of 0's, 1's and 2's.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+ {
+    public:
+    void sort012(int a[], int n)
+    {
+        // code here 
+       int low=0;
+       int mid=0;
+       int high=n-1;
+       
+       while(mid<=high)
+       {
+           if(a[mid]==0)
+           {
+               swap(a[mid], a[low]);
+               mid++;
+               low++;
+           }
+           else if(a[mid]==1)
+           {
+                mid++;   
+           }
+           else
+           {
+               swap(a[mid], a[high]);
+               high--;
+           }
+       }
+    }
+};
