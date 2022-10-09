e2c14d14746d3270c90e124b449781f3bb56e5cf
sadhvibs <76248658+sadhvibs@users.noreply.github.com>
2022-10-09 21:49:36 +0530

    .init

---
+++ GFG/Basic/Print elements.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+public:
+    //Just print the space seperated array elements
+	void printArray(int arr[], int n) {
+	    // code here
+	    for(int i=0; i<n; i++){
+	        cout<<arr[i]<<" ";
+	    }
+	}
+};
