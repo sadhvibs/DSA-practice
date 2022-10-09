c3325f4983aee221698825a7b15b134a7c34091f
sadhvibs <76248658+sadhvibs@users.noreply.github.com>
2022-10-09 20:36:33 +0530

    .init

---
+++ GFG/Basic/Sum of series.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+public:
+	// function to return sum of  1, 2, ... n
+	long long seriesSum(int n) {
+	    // code here
+        return (long)n*(n+1)/2;
+	}
+};
