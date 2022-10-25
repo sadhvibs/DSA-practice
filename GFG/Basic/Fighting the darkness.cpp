54a00b9f59a617b4b2a9bb312fc5d00e673fcfeb
sadhvibs <76248658+sadhvibs@users.noreply.github.com>
2022-10-25 09:45:16 +0530

    .init

---
+++ GFG/Basic/Fighting the darkness.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Solution{   
+public:
+    int maxDays(int arr[],int n){
+        // code here   
+        int max=0;
+        for(int i=0; i<n; i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+};
