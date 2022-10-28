0ca829a54f01a668dfaa432ca74ef9c6047ba40a
sadhvibs <76248658+sadhvibs@users.noreply.github.com>
2022-10-28 15:53:39 +0530

    .init

---
+++ GFG/Basic/Convert array into zig-zag fashion.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+  public:
+    // Program for zig-zag conversion of array
+    void zigZag(int arr[], int n) {
+        // code here
+        
+        for(int i=0; i<n-1; i++){
+            if(i%2==0){
+                if(arr[i]>arr[i+1]){
+                    swap(arr[i], arr[i+1]);
+                }
+            }
+            
+            if(i%2!=0){
+                if(arr[i]<arr[i+1]){
+                    swap(arr[i], arr[i+1]);
+                }
+            }
+        }
+    }
+};
