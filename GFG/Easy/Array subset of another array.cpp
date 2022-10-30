6836c38c652a63fd8b43073654a944448e85f9af
sadhvibs <76248658+sadhvibs@users.noreply.github.com>
2022-10-30 19:32:46 +0530

    .init

---
+++ GFG/Easy/Array subset of another array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+string isSubset(int a1[], int a2[], int n, int m) {
+    //code here
+    sort(a1, a1+n);
+    sort(a2, a2+m);
+    
+    int i=0, j=0;
+    while(i<n && j<m){
+        if(a1[i]==a2[j])
+            j++;
+            i++;
+    }
+    if(j==m)
+        return "Yes";
+    else
+        return "No";
+}
